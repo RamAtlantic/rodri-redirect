eec418a2328cdfeef18e772ce8511d02c818378e
RamAtlantic <arceramirom@gmail.com>
2025-08-25 20:43:41 -0300

    cambios rodri pxiel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: 'Redirect ORO PURO',
-  description: 'Redirect ORO PURO',
-  generator: 'ORO PURO',
-}
+  title: "Redirect ORO PURO",
+  description: "Redirect ORO PURO",
+  generator: "ORO PURO",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -25,7 +26,50 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !(function (f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function () {
+                  n.callMethod
+                    ? n.callMethod.apply(n, arguments)
+                    : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = "2.0";
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+              })(
+                window,
+                document,
+                "script",
+                "https://connect.facebook.net/en_US/fbevents.js"
+              );
+              fbq("init", "${process.env.NEXT_PUBLIC_META_PIXEL_ID}");
+              fbq("track", "PageView");
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
